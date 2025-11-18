00c1c5e6469e15f04c97af8acd3e0ae0cb4962a1
Quizz1e <123racers123@gmail.com>
2025-11-18 23:04:54 +0300

    Update __init__.py

--- pymc_bart/__init__.py
+++ pymc_bart/__init__.py
@@ -19,6 +19,9 @@ from pymc_bart.pgbart import PGBART
 from pymc_bart.dtbart import DecisionTableBART, DTBART
 from pymc_bart.decision_table import DecisionTable 
 
+from .dtbart import DecisionTableBART, DTBART
+__all__.extend(["DecisionTableBART", "DTBART"])
+
 # Split rules и утилиты
 from pymc_bart.split_rules import (
     ContinuousSplitRule,
